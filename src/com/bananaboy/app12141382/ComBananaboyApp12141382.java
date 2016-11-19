16f7fa99c5c92c665cd380460462b6f6d7bcb0b9
setyawanrudi120@gmail.com <setyawanrudi120@gmail.com>
2016-11-20 00:05:31 +0700

    12141382 ->  Pertama saya membuat koding awal untuk inputan menggunakan scanner, dan fungsi do while untuk membuat case permainan atau tidak

---
+++ src/com/bananaboy/app12141382/ComBananaboyApp12141382.java
@@ -0,0 +1,23 @@
+package com.bananaboy.app12141382;
+
+import java.util.Scanner;
+
+
+public class ComBananaboyApp12141382 {
+
+    
+    public static void main(String[] args) {
+        
+    game main = new game();
+    
+    Scanner inputString = new Scanner(System.in);
+    Scanner inputAngka = new Scanner(System.in);
+            
+        do{
+            System.out.println("Masukkam nama anda untuk memulai game atau tekan 'T' untuk keluar");
+            main.nama=String.nextLine();
+          }
+        while(!"T.equals(main.nama");
+    }
+      
+}

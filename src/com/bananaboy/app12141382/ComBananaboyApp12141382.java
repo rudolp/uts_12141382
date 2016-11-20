93b9f2f7b23baafbc751aee857e956b19ef5b2ae
setyawanrudi120@gmail.com <setyawanrudi120@gmail.com>
2016-11-20 17:13:49 +0700

    11421238 -> Langkah kedua saya membuat panduan untug game, berikut sisa kesempatan dan poin yang didapat dengan menambahkan fungsi if else.

--- src/com/bananaboy/app12141382/ComBananaboyApp12141382.java
+++ src/com/bananaboy/app12141382/ComBananaboyApp12141382.java
@@ -16,8 +16,24 @@ public class ComBananaboyApp12141382 {
         do{
             System.out.println("Masukkam nama anda untuk memulai game atau tekan 'T' untuk keluar");
             main.nama=String.nextLine();
-          }
-        while(!"T.equals(main.nama");
+        
+            if("T".equals(main.nama)){
+                break;
+            }
+            else{
+                do{
+                    main.random=(short)(Math.random()*main.Level());
+                    System.out.printl("#Level"+main.level);
+                    System.out.println("Selamat Datang"+main.nama);
+                    System.out.printl("Silahkan Tebak angka antara 1 s/d "+main.Level());
+                    System.out.printl("Anda masih memiliki kesempatan menebak sebanyak"+main.kesempatan+"kali");
+                    main.sisa=main.kesempatan;
+                }
+            
+            }
+        
+        
+        }while(!"T".equals(main.nama));
     }
       
 }

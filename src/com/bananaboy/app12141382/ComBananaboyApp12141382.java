05a222174c327be7fb00f43f97dbe94c7cc06090
setyawanrudi120@gmail.com <setyawanrudi120@gmail.com>
2016-11-20 17:23:32 +0700

    11421238 -> Kemudian saya membuat untuk inputan tebakan dan langsung membandingkan jawaban tebakan debgan hasil angka acak dengan fungsi do while
    12141382 -> selanjutnya dengan menambahkan fungsi if else yang saya gunakan untuk meningkankan level permainan  jika jawaban bener dan selesai jika jawaban  salah dan kesempatan mencoba sudah selesai
    
    12141382 -> KEmudian menambahkan fngsi do while untuk memulai kembali permainan dengan level yang lebih tinggi
    
    12141382 -> Kali ini saya membuat class baru bernama class game untuk menginisialisasi.
    
    12141382 -> Sekarang saya pindahkan isi class untuk inisialisasi keclass utama,

--- src/com/bananaboy/app12141382/ComBananaboyApp12141382.java
+++ src/com/bananaboy/app12141382/ComBananaboyApp12141382.java
@@ -4,7 +4,14 @@ import java.util.Scanner;
 
 
 public class ComBananaboyApp12141382 {
-
+    String nama;
+    byte level=1;
+    byte kesempatan=5,sisa;
+    short banyak;
+    short tebak;
+    int inputAngka;
+    int random, score=0;
+    char jawab, pilih;
     
     public static void main(String[] args) {
         
@@ -22,14 +29,33 @@ public class ComBananaboyApp12141382 {
             }
             else{
                 do{
-                    main.random=(short)(Math.random()*main.Level());
-                    System.out.printl("#Level"+main.level);
+                    main.random=(short)(Math.random()*main.level());
+                    System.out.println("#Level"+main.level);
                     System.out.println("Selamat Datang"+main.nama);
-                    System.out.printl("Silahkan Tebak angka antara 1 s/d "+main.Level());
-                    System.out.printl("Anda masih memiliki kesempatan menebak sebanyak"+main.kesempatan+"kali");
+                    System.out.println("Silahkan Tebak angka antara 1 s/d "+main.level());
+                    System.out.println("Anda masih memiliki kesempatan menebak sebanyak"+main.kesempatan+"kali");
                     main.sisa=main.kesempatan;
-                }
-            
+                    
+                    do{
+                        System.out.println();
+                        System.out.println("Silahkan Masukkan Tebakan anda = ");
+                        main.tebak=InputAngka.nextShort();
+                        main.hasilmenebak();
+                        
+                        }while(main.tebak!=main.random&&main.sisa>0);
+                    
+                    if(main.tebak==main.random){
+                        main.nilai();
+                        System.out.println("Apakah anda ingin melanjutkan ke level berikutnya?y/t");
+                        main.jawab=inputString.nextLine().charAt(0);
+                        main.level++;
+                        main.banyak=0;
+                    
+                        }else{break;}
+                    
+                }while(main.jawab=='y');
+                main.totalNilai();
+                main.Reset();
             }
         
         
---
+++ src/com/bananaboy/app12141382/Game.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.bananaboy.app12141382;
+
+/**
+ *
+ * @author windows10
+ */
+class Game {
+    String nama;
+    byte level=1;
+    byte kesempatan=5,sisa;
+    short banyak;
+    short tebak;
+    int random, score=0;
+    char jawab, pilih;
+       
+    
+}
